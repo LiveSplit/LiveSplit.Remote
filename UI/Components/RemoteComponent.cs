bc005c2f4d72d0106d5293ee451617a8c4f21281
Christopher Serr <cryze92@gmail.com>
2015-05-09 20:56:36 +0200

    Added the Component and Factory classes

---
+++ UI/Components/RemoteComponent.cs
@@ -0,0 +1,40 @@
+﻿using LiveSplit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LiveSplit.UI.Components
+{
+    public class RemoteComponent : LogicComponent
+    {
+        public override string ComponentName
+        {
+            get { return "LiveSplit Remote"; }
+        }
+
+        public override Control GetSettingsControl(LayoutMode mode)
+        {
+            return null;
+        }
+
+        public override XmlNode GetSettings(XmlDocument document)
+        {
+            return document.CreateElement("settings");
+        }
+
+        public override void SetSettings(XmlNode settings)
+        {
+        }
+
+        public override void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+        }
+
+        public override void Dispose()
+        {
+        }
+    }
+}
---
+++ UI/Components/RemoteFactory.cs
@@ -0,0 +1,51 @@
+﻿using LiveSplit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LiveSplit.UI.Components
+{
+    public class RemoteFactory : IComponentFactory
+    {
+        public string ComponentName
+        {
+            get { return "LiveSplit Remote"; }
+        }
+
+        public string Description
+        {
+            get { return "LiveSplit Remote allows you to control LiveSplit from a web based timer."; }
+        }
+
+        public ComponentCategory Category
+        {
+            get { return ComponentCategory.Control; }
+        }
+
+        public IComponent Create(LiveSplitState state)
+        {
+            return new RemoteComponent();
+        }
+
+        public string UpdateName
+        {
+            get { return ComponentName; }
+        }
+
+        public string UpdateURL
+        {
+            get { return "http://livesplit.org/update/"; }
+        }
+
+        public Version Version
+        {
+            get { return Version.Parse("1.0.0"); }
+        }
+
+        public string XMLURL
+        {
+            get { return "http://livesplit.org/update/Components/LiveSplit.Remote.xml"; }
+        }
+    }
+}
